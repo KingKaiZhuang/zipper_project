25e03a6f7f4b6a83821604df53451c8fb3428e58
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-12-07 19:59:40 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,6 @@
 const express = require('express');
-// const http = require('http');
 const path = require('path');
-// const socketIO = require('socket.io');
 const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server);
 
 const zipperRouter = require('./router/ZipperRouter');
 app.use("/zipper", zipperRouter);
@@ -19,17 +15,7 @@ app.get('/', (req, res) => {
     console.log(123);
 });
 
-// io.on('connection', (socket) => {
-//     console.log('A user connected');
-    
-//     socket.on('disconnect', () => {
-//         console.log('User disconnected');
-//     });
-// });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// module.exports = { io };
